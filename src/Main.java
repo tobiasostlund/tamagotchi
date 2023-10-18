2d74e75b345f91cdfebcca874678c89136409ea0
tobiasostlund <tobias.ostlund@elev.ga.ntig.se>
2023-10-18 11:05:20 +0200

    Saker och grejer

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,44 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+    //för att färga texten
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        ArrayList<String> Tamagotchis=new ArrayList<String>();
+        ArrayList<Tamagotchi> tamagotchis=new ArrayList<Tamagotchi>();
         System.out.println("Välkommen till Tamagotchi!");
         System.out.println("Vad ska din Tamagotchi heta? ");
         String name=scan.nextLine();
         Tamagotchi tama1 = new Tamagotchi(name);
+        tamagotchis.add(tama1);
         System.out.println(tama1.name+" är ett jätte dåligt namn wtf");
+        System.out.println();
 
+        int money = 50;
 
         while (true){
+
             System.out.println("Skriv numret till valet du vill göra:");
-            System.out.println("1: Mata");
-            System.out.println("2: Lär ett nytt ord");
+            System.out.println("1: Mata ("+(money)+ANSI_RED+"-10 kr"+ANSI_RESET+")");
+            System.out.println("2: Lär ett nytt ord ("+(money)+ANSI_RED+"-10 kr"+ANSI_RESET+")");
             System.out.println("3: Säg ett ord");
-            System.out.println("4: Gör ingenting");
+            System.out.println("4: Sälj en njure för pengar ("+money+ANSI_GREEN+"+50 kr"+ANSI_RESET+")");
+            System.out.println("5: Gör ingenting");
             int menyVal = scan.nextInt();
 
             switch(menyVal){
                 case 1:
+                    money=money-10;
                     tama1.feed();
                     tama1.printStats();
                     break;
                 case 2:
+                    money=money-10;
                     scan.nextLine();
                     System.out.println("Vad för ord ska Tamagotchin lära sig?");
                     String newWord = scan.nextLine();
@@ -40,6 +53,8 @@ public class Main {
                     tama1.printStats();
                     break;
                 case 4:
+
+                case 5:
                     System.out.println("Du gjorde ingenting...");
                     tama1.printStats();
                     break;
